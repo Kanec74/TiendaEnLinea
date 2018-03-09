57a552e63ee1b9b707225d5f9ba5c8442c305422
izeth <yakzil_20@hotmail.com>
2018-03-09 12:10:13 -0600

    Vista del Carrito de compras

--- src/js/carritodecompra/controllers/CarritoDeCompraController.js
+++ src/js/carritodecompra/controllers/CarritoDeCompraController.js
@@ -16,17 +16,33 @@ function CarritoDeCompraController($scope, $localStorage){
 	}
 
 	function _newItemCarrito(){
-		return { nombre: '', id: ''}
+		return { productoId:'123', name: 'Producto', price: '100.00', lot:'2.00', total:'200'}
 	}
 
 	$scope.removeItemCarrito = function(index){
 		$scope.$storage.carrito.splice(index,1)	
+		$scope.totalCompra = _getTotal()
 	}
 
 	$scope.editarItemCarrito = function(index){
 		let itemcarrito = $scope.$storage.carrito.splice(index,1)
 		$scope.itemcarrito = itemcarrito[0]
 	} 
+
+	$scope.comprar = function(){
+		$scope.$storage.carrito = []
+		$scope.totalCompra = 0
+	} 
+
+	function _getTotal(){
+		var totalCompra=0;
+		for (var i = 0; i < $scope.$storage.carrito.length; i++) {
+	        totalCompra = parseFloat(totalCompra) + parseFloat($scope.$storage.carrito[i].total);
+	    }
+		return totalCompra
+	}
+
+    $scope.totalCompra = _getTotal()
 }
 
 module.exports = CarritoDeCompraController
