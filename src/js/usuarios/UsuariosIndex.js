ac08454c7652962e0a6f68a28b569c1ba3b254c4
rickyarellano <ricardo-arellano@outlook.com>
2018-03-09 20:08:27 -0600

    Usuarios

--- src/js/usuarios/UsuariosIndex.js
+++ src/js/usuarios/UsuariosIndex.js
@@ -6,25 +6,36 @@ angular.module('app.usuarios', ['ui.router'])
 			url:'/usuarios',
 			template: '<ui-view />',
 			abstract:true
-
 		})
-		.state({
-			name: 'app.usuarios.index',
-			url:'',
-			templateUrl: '/views/usuarios/usuarios.html',
-			controller: 'UsuariosController'
-			})
-		.state({
-			name: 'app.usuarios.detalle',
-			url:'/{username}',
-			params:{email:''},
-			templateUrl: '/views/usuarios/profile.html',
-			controller: 'ProfileController'
+					.state({
+						name: 'app.usuarios.index',
+						url:'',
+						templateUrl: '/views/usuarios/usuarios.html',
+						controller: 'UsuariosController'
+						})
+					.state({
+						name: 'app.usuarios.detalle',
+						url:'/{username}',
+						params:{email:''},
+						templateUrl: '/views/usuarios/profile.html',
+						controller: 'ProfileController'
+						})
+						.state({
+							name: 'app.usuarios.añadir',
+							url:'',
+							templateUrl:'/views/usuarios/crear_usuario.html',
+							controller: 'UsuariosController'
+						})
+						.state({
+							name: 'app.usuarios.editar',
+							url: '',
+							templateUrl: '/views/usuarios/editar_usuario.html',
+							controller: 'UsuariosController'
+						})
 
-		})
+
 
 	}])
 .controller('UsuariosController', require('./controllers/UsuariosController'))
 
 .controller('ProfileController', require('./controllers/ProfileController'))
-
--- src/js/usuarios/controllers/UsuariosController.js
+++ src/js/usuarios/controllers/UsuariosController.js
@@ -7,47 +7,26 @@ function UsuariosController($scope, $localStorage){
 	//$scope.user = {}
 	$scope.user = _newUser()
 
-	/*$scope.$storage.usuarios = [
-		{
-			nombre: "Kanec",
-			email: "kanec@uady.mx",
-			username: "khc"
-		},
-		{
-			nombre: "Luis",
-			email: "luis@uady.mx",
-			username: "lu"
-		},
-		{
-			nombre: "Pepe",
-			email: "pepe@uady.mx",
-			username: "pe"
-		},
-		{
-			nombre: "Felipe",
-			email: "felipe@uady.mx",
-			username: "fe"
-		}
-	]*/
-
 	$scope.addUsuario = function(){
-		let usuario = angular.copy($scope.user)
-		$scope.$storage.usuarios.push(usuario)
+		let usuario1 = angular.copy($scope.user)
+		$scope.$storage.usuarios.push(usuario1)
 		$scope.user = _newUser()
+
 	}
 
 	function _newUser(){
-		return { nombre: '', email: ''}
+		return { nombre: '', email: '', username: '', password: ''}
 	}
 
 	$scope.removeUsuario = function(index){
-		$scope.$storage.usuarios.splice(index,1)	
+
+		$scope.$storage.usuarios.splice(index,1)
 	}
 
 	$scope.editarUsuario = function(index){
 		let user = $scope.$storage.usuarios.splice(index,1)
 		$scope.user = user[0]
-	} 
+	}
 }
 
 UsuariosController.$inject = ['$scope', '$localStorage']
